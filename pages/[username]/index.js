54e27df81977a805bb7a823c6de61d5e6d76cc01
scare-destiny <zhenya.venger@gmail.com>
2022-10-18 13:14:21 +0300

    test

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,18 +1,21 @@
-import UserProfile from '../../components/UserProfile'
-import PostFeed from '../../components/PostFeed'
-import { getUserWithUsername, postToJSON } from '../../lib/firebase'
+import { getUserWithUsername, postToJSON } from '@lib/firebase'
+import UserProfile from '@components/UserProfile'
+import Metatags from '@components/Metatags'
+import PostFeed from '@components/PostFeed'
 
 export async function getServerSideProps({ query }) {
 	const { username } = query
 
 	const userDoc = await getUserWithUsername(username)
 
+	// If no user, short circuit to 404 page
 	if (!userDoc) {
 		return {
 			notFound: true,
 		}
 	}
 
+	// JSON serializable data
 	let user = null
 	let posts = null
 
@@ -23,17 +26,21 @@ export async function getServerSideProps({ query }) {
 			.where('published', '==', true)
 			.orderBy('createdAt', 'desc')
 			.limit(5)
-
 		posts = (await postsQuery.get()).docs.map(postToJSON)
 	}
+
 	return {
-		props: { user, posts },
+		props: { user, posts }, // will be passed to the page component as props
 	}
 }
 
 export default function UserProfilePage({ user, posts }) {
 	return (
 		<main>
+			<Metatags
+				title={user.username}
+				description={`${user.username}'s public profile`}
+			/>
 			<UserProfile user={user} />
 			<PostFeed posts={posts} />
 		</main>
